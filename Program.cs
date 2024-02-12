f076caa55a3f9321706123f8ab6964cb4925bc83
Nurkhissa <45072957+Nurkhissa@users.noreply.github.com>
2024-02-12 20:48:42 +0500

    Upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Console.Write("Enter a number: ");
+string inputNumber = Console.ReadLine();
+int number = Convert.ToInt32(inputNumber);
+
+bool isPrime = true;
+int count = number/2;
+for(int i=2; i<=count; i++)
+{
+    if(number%i==0)
+    {
+        isPrime=false;
+        break;
+    }
+}
+
+if(isPrime)
+{
+    System.Console.WriteLine("This is prime number");
+}
+else
+{
+    System.Console.WriteLine("This is not prime number");
+}
+
